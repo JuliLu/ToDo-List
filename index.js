6c55f1d0baa944ead433e6e3a1f50b15157b0512
Julia <julialukyanova22.91@gmail.com>
2022-09-09 18:09:50 +0300

    function todo

---
+++ index.js
@@ -0,0 +1,18 @@
+let todos = [];
+
+function addToDo(text) {
+    const todo = {
+        text,
+        done: false,
+        id: `${Math.random()}`
+    };
+    todos.push(todo);
+}
+
+function deleteTodo(id){
+    todos.forEach(todo => {
+        if (todo.id === id){
+            todo.done = true;
+        }
+    })
+}
